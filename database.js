b45f724623dcf1f6a29100baa0a112e48a78efe5
omaymadjebali <omayma.djebali@esprit.tn>
2022-08-27 12:57:53 +0100

    nchalah temchi aad

---
+++ database.js
@@ -0,0 +1,10 @@
+database.connect = async () => {
+    /**
+ * Import MongoClient & connexion à la DB
+ */
+    const MongoClient = require('mongodb').MongoClient;
+    const url = 'mongodb://mongo:27017/parkingApi';
+    let client = await MongoClient.connect(url,{ useNewUrlParser: true });
+    let db = client.db('parkingApi');
+    database.db = db
+}
